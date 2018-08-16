7a9423b1b396206c391bd1234dec31828a972d74
Zolboo <33338165+zolbooo@users.noreply.github.com>
2018-08-16 22:07:31 +0800

    (if) now binds only to valid var names, uses eval() otherwise

--- src/kachjs/directives/if.ts
+++ src/kachjs/directives/if.ts
@@ -1,13 +1,13 @@
+/// <reference path="../varcheck.ts"/>
 class KachIfDirective {
-  constructor(el: HTMLElement, arg: string, not?: boolean) {
-    try {
-      eval('(function(' + arg + '){})');
-    } catch {
-      el.hidden = eval(arg);
-      return;
+  constructor(el: HTMLElement, arg: string, invert?: boolean) {
+    if (isValidVarName(arg)) {
+      bind(arg);
+      el.hidden = invert ? $data[arg] : !$data[arg];
+      $subscribes[arg].push(() => (el.hidden = invert ? $data[arg] : !$data[arg]));
+    } else {
+      let ev = eval(arg);
+      el.hidden = invert ? ev : !ev;
     }
-    bind(arg);
-    el.hidden = not ? $data[arg] : !$data[arg];
-    $subscribes[arg].push(() => (el.hidden = not ? $data[arg] : !$data[arg]));
   }
 }
